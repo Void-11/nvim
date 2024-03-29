8c38d96da88a02c6df967a1d721f5dfb730002a9
Void-11 <vivaanmishra777@gmail.com>
2024-03-29 19:19:29 +0530

    Added Alpha.nvim and Completions

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,12 +1,17 @@
-return{
+return {
   "neovim/nvim-lspconfig",
   config = function()
-    local lspconfig = require ("lspconfig")
-    lspconfig.lua_ls.setup({})
-    lspconfig.clangd.setup({})
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local lspconfig = require("lspconfig")
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities  
+    })
+    lspconfig.clangd.setup({
+      capabilities = capabilities
+    })
 
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-    vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
   end
 }
